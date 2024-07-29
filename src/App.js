9aa276c57904d581d1fb98e480ca644d6fe75c8e
Chaitanyatraining <chaithanya5421@gmail.com>
2024-07-29 09:00:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+import ClassComp from './ClassComp';
+import ListandKeys from './Components/ListsandKeys';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <h2>Welcome to React</h2>
+     <Header />
+     <ClassComp /> */}
+     <ListandKeys />
+    </div>
+  );
+}
+
+export default App;
